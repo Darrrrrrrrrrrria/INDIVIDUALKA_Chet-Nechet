65faa31663479239e747255e223058c89f954853
Darrrrrrrrrrrria <161242102+Darrrrrrrrrrrria@users.noreply.github.com>
2024-03-18 13:00:22 +0300

    Source.cpp

--- INDIVIDUALKA_Chet-Nechet/Source.cpp
+++ INDIVIDUALKA_Chet-Nechet/Source.cpp
@@ -8,7 +8,7 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	int n;
-	do { cout << "Âåäèòå N- ðàçìåðòîñòü ïîëÿ. Îáðàòèòå âíèìàíèå 2<=N<=10"; cin >> n; } while (n < 2 || n>10);
+	do { cout << "Введите N-размерность поля.Обратите внимание: 2<=N<=10  "; cin >> n; } while (n < 2 || n>10);
 	system("color C");
 	play(n);
 	int a;
